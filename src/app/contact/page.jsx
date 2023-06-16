947e8a931241625c754ef85f5fe0e70a73e0cd39
andar <andar.orion@gmail.com>
2023-06-17 04:07:28 +0700

    contact & portfolio creation

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import style from './page.module.css'
+import Image from 'next/image'
+import Button from '@/components/Button/Button'
 
 const Contact = () => {
   return (
-    <div className={style.container} >Contact</div>
+    <div className={style.container}>
+      <h1 className={style.title} >Lets keep in touch</h1>
+      <div className={style.content}> 
+        <div className={style.imgContainer}>
+          <Image 
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={style.image}
+          />
+        </div>
+        <form className={style.form} >
+          <input type="text" placeholder='name' className={style.input} />
+          <input type="text" placeholder='email' className={style.input} />
+          <textarea className={style.textarea} placeholder='message' id="" cols="30" rows="10"></textarea>
+          <Button url='#' text="Send" />
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import style from './page.module.css'
+import Link from 'next/link'
 
 const Portfolio = () => {
   return (
-    <div className={style.container}>Portfolio</div>
+    <div className={style.container}>
+      <h1 className={style.selectTitle} >Choose a gallery</h1>
+      <div className={style.items}>
+        <Link href='/portfolio/illustration' style={{background: "url('/illustration.png')"}} className={style.item} >
+          <span className={style.title} >Illustration</span>
+        </Link>
+        <Link href='/portfolio/website' style={{background: "url('/websites.jpg')"}} className={style.item} >
+          <span className={style.title} >Website</span>
+        </Link>
+        <Link href='/portfolio/applications' style={{background: "url('/apps.jpg')"}} className={style.item} >
+          <span className={style.title} >Applications</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
