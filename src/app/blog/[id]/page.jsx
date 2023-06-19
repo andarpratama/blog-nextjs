9b5f7ddb4d0d515a8dc04616f64fff5cba493cd4
andar <andar.orion@gmail.com>
2023-06-20 00:54:51 +0700

    blog page creation

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import style from './page.module.css';
+import Image from 'next/image';
 
-const BlogPost = () => {
+const BlogPost = ({ params }) => {
   return (
-    <div>BlogPost</div>
-  )
-}
+    <div className={style.container}>
+      <div className={style.top}>
+        <div className={style.info}>
+          <h1 className={style.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+            voluptatem?`
+          </h1>
+          <p className={style.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic libero,
+            expedita ullam vitae pariatur, atque at dignissimos error labore
+            incidunt excepturi? Dicta, itaque natus? Blanditiis dolores
+            asperiores modi dignissimos dolorum?
+          </p>
+          <div className={style.author}>
+            <Image
+              src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
+              alt=''
+              width={40}
+              height={40}
+              className={style.avatar}
+            />
+            <span className={style.username}>Username</span>
+          </div>
+        </div>
+        <div className={style.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
+            alt=''
+            fill={true}
+            className={style.image}
+          />
+        </div>
+      </div>
+      <div className={style.content}>
+        <p className={style.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+          architecto debitis non beatae sequi iste eius assumenda esse quibusdam
+          tempora facilis aperiam unde tempore harum libero, culpa quis quod
+          vero.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default BlogPost
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import style from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Blog = () => {
   return (
-    <div className={style.container} >Blog</div>
+    <div className={style.container} >
+      <Link href='/blog/testid1' className={style.container} >
+        <div className={style.imgContainer}>
+          <Image
+            src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
+            alt=''
+            width={400}
+            height={250}
+            className={style.image}
+          />
+        </div>
+        <div className={style.content}>
+          <h1 className={style.title}>test</h1>
+          <p className={style.desc}>test</p>
+        </div>
+      </Link>
+      <Link href='/blog/testid2' className={style.container} >
+        <div className={style.imgContainer}>
+          <Image
+            src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
+            alt=''
+            width={400}
+            height={250}
+            className={style.image}
+          />
+        </div>
+        <div className={style.content}>
+          <h1 className={style.title}>test</h1>
+          <p className={style.desc}>test</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
